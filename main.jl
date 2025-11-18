5390b8a0ef45a5a08a1542bc0c955813b7db315d
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-11-18 10:48:55 -0600

    Exploring trade

--- main.jl
+++ main.jl
@@ -1,16 +1,7 @@
-using WiNDCContainer
+#using WiNDCContainer
 using WiNDCCanada
 
 using DataFrames, CSV, XLSX
-
-
-
-
-file_path = raw"data\xlsx\AB_SUT_C2022_S.xlsx"
-
-X = build_canada_province_table(file_path, :AB)
-
-
 
 X = build_canada_table("data/xlsx")
 
@@ -38,6 +29,65 @@ table(X, :Value_Added) |>
     x -> groupby(x, [:year]) |>
     x -> combine(x, :value => sum => :value) 
 
-table(X, :Final_Demand, :Import) |>
+table(X, :Final_Demand, :Import) |> 
     x -> groupby(x, [:year]) |>
     x -> combine(x, :value => sum => :value)
+    
+
+outerjoin(
+    table(X, :Interprovincial_Import) |> x -> select(x, :row, :column => :source, :province => :destination, :value => :import),
+    table(X, :Interprovincial_Export) |> x -> select(x, :row, :column => :destination, :province => :source, :value => :export),
+    on = [:row, :source, :destination],
+) |>
+x -> transform(x, 
+    [:import, :export] => ByRow(+) => :diff
+) |>
+x -> sort(x, :diff) 
+
+
+table(X, :Interprovincial_Import, :Interprovincial_Export) |>
+    x -> groupby(x, [:row, :year, :column, :province]) |>
+    x -> combine(x, :value => sum => :value) |>
+    x -> sort(x, :value)
+
+
+table(X, :Export)
+table(X, :ReExport)
+table(X, :Import)
+
+
+table(X, 
+    :Interprovincial_Export, 
+    #:Interprovincial_Import, 
+    :commodity => :M1140,
+    #:trade_province => [:AB, :NL],
+    #:province => [:AB, :NL]
+    )
+
+
+elements(X, :province)
+
+table(X, :Intermediate_Demand, :Other_Final_Demand, :ReExport, :Import, :Interprovincial_Import) |>
+    x -> groupby(x, [:row, :year, :province]) |>
+    x -> combine(x, :value => sum => :value) |>
+    x -> subset(x,
+        :value => ByRow(>(1e-5)) 
+    ) |>
+    x -> leftjoin(
+        x,
+        elements(X, :commodity) |> y -> select(y, :name, :description),
+        on = :row => :name
+    )
+
+
+
+
+table(X, :Interprovincial_Import) |>
+    x -> sort(x, :value)
+
+elements(X, :commodity)
+
+table(X, :pce) |>
+    x -> groupby(x, [:row, :province, :year, :parameter]) |>
+    x -> combine(x, :value => sum => :value) #|>
+    x -> subset(x, :value => ByRow(>(0)))  
